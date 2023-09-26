af00646be9ce681829769f7c8c3d1aa96bad7485
oliwia-paliwoda <olipal001@pbs.edu.pl>
2023-09-26 19:27:44 +0200

    test3

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,17 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-calculator/' : '/',
-};
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/vue-calculator/',
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
